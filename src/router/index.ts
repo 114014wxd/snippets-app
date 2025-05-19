eccf2fd89a1810fe44901487bd916a882eabd5c0
114014wxd <138952378+114014wxd@users.noreply.github.com>
2025-05-19 15:46:03 +0800

    部署

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '@/views/Home/Home.vue'
 import ReactView from '@/views/Snippet/React.vue'
 import ArrayView from '@/views/Snippet/Array.vue'
@@ -38,7 +38,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
