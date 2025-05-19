12f91ab31c28947826b1e2dd6454b400e02ef063
114014wxd <138952378+114014wxd@users.noreply.github.com>
2025-05-19 10:26:02 +0800

    排查问题

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,16 +1,19 @@
-import common from './modules/common'
-import sidebar from './modules/sidebar'
-import snippet from './modules/snippet'
+import zhSidebar from './modules/sidebar'
+import enSidebar from './modules/sidebar'
+import zhCommon from './modules/common'
+import enCommon from './modules/common'
+import zhSnippet from './modules/snippet'
+import enSnippet from './modules/snippet'
 
 export default {
   zh: {
-    common: common.zh,
-    sidebar: sidebar.zh,
-    snippet: snippet.zh,
+    sidebar: zhSidebar.zh,
+    common: zhCommon.zh,
+    snippet: zhSnippet.zh,
   },
   en: {
-    common: common.en,
-    sidebar: sidebar.en,
-    snippet: snippet.en,
-  }
+    sidebar: enSidebar.en,
+    common: enCommon.en,
+    snippet: enSnippet.en,
+  },
 }
