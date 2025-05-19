0978eb5175038e364fb33f69f4ee65f08f3ce35c
114014wxd <138952378+114014wxd@users.noreply.github.com>
2025-05-19 15:28:22 +0800

    带娃

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,8 +3,7 @@ import messages from './locales'
 
 export const i18n = createI18n({
   legacy: false,
-  globalInjection: true,
   locale: 'zh',
-  fallbackLocale: 'en',
-  messages
+  messages,
+  globalInjection: true
 })
