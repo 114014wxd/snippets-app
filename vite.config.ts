d1f029f82695dd7da3f247fd75e7105c1120fa65
114014wxd <138952378+114014wxd@users.noreply.github.com>
2025-05-19 08:30:18 +0800

    修改bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/locales',
+          dest: '' // 拷贝到 dist 根目录
+        }
+      ]
+    })
+  ],
   base: './',
   resolve: {
     alias: {
