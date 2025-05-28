07772c85e42c5c254450a1763ec7bece56c118dc
114014wxd <138952378+114014wxd@users.noreply.github.com>
2025-05-28 14:53:35 +0800

    验证

--- src/libs/appwrite.ts
+++ src/libs/appwrite.ts
@@ -2,8 +2,13 @@ import { Client, Account } from 'appwrite'
 
 const client = new Client()
 
+const endpoint = import.meta.env.VITE_APPWRITE_ENDPOINT
+const fullEndpoint = endpoint.startsWith('/')
+  ? `${window.location.origin}${endpoint}` // ✅ 拼接完整地址
+  : endpoint
+
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setEndpoint(fullEndpoint)
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
 
 const account = new Account(client)
